edc4b9d2cb754a4ed96fe9a59b3befda9c647f8c
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-08-27 21:47:26 +0900

    8.アンケートフォームを修正しよう

--- src/public/index.php
+++ src/public/index.php
@@ -1,21 +1,41 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>トップページ</title>
-    </head>
-    <body>
-        <form action="complete.php" method="post">
-            回答者名
-            <input type="text" name="name" value=""><br />
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>トップページ</title>
+    <!-- Tailwind CSS を読み込む -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+</head>
 
-            好きな食べ物をご記入下さい
-            <input type="text" name="food_awnser" value=""><br />
-
-            趣味をご記入下さい
-            <input type="text" name="hobby_awnser" value=""><br />
-
-            <input type="submit" value="送信" />
-        </form>
-    </body>
+<body>
+    <section class="text-gray-600 body-font relative">
+        <div class="container px-5 py-24 mx-auto">
+            <div class="lg:w-1/2 md:w-2/3 mx-auto">
+                <form action="complete.php" method="post" class="flex flex-wrap -m-2">
+                    <div class="p-2 w-1/2">
+                        <div class="relative">
+                            <label for="name" class="leading-7 text-sm text-gray-600">回答者名</label>
+                            <input type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                        </div>
+                    </div>
+                    <div class="p-2 w-1/2">
+                        <div class="relative">
+                            <label for="food_awnser" class="leading-7 text-sm text-gray-600">好きな食べ物をご記入してしてください</label>
+                            <input type="food_awnser" id="food_awnser" name="food_awnser" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                        </div>
+                    </div>
+                    <div class="p-2 w-full">
+                        <div class="relative">
+                            <label for="hobby_awnser" class="leading-7 text-sm text-gray-600">趣味をご記入ください</label>
+                            <textarea id="hobby_awnser" name="hobby_awnser" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                        </div>
+                    </div>
+                    <div class="p-2 w-full">
+                    <button class="flex mx-auto text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg" type="submit">送信</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+</body>
 </html>
